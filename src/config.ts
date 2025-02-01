9a7222b3a932f0fbf772a27aa61bd52547460df2
Manav <manav18gadhiya@gmail.com>
2025-02-01 23:06:13 +0530

    add: logging & middleware

--- src/config.ts
+++ src/config.ts
@@ -3,3 +3,4 @@ import { configDotenv } from "dotenv";
 configDotenv();
 
 export const PORT = process.env.PORT || 3000;
+export const LOGGING_DIRECTORY = "./logs/";
--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,12 @@
 import express, { urlencoded } from "express";
 import cors from "cors";
 import helmet from "helmet";
+import compression from "compression";
 import { PORT } from "./config";
 import router from "./routes";
 import { handleErrors } from "./middlewares/errorHandler";
 import { limiter } from "./middlewares/rateLimiter";
+import { loggingMiddleware } from "./middlewares/logger";
 
 const app = express();
 
@@ -14,6 +16,8 @@ app.use(cors());
 app.use(express.json());
 app.use(urlencoded({ extended: false }));
 app.use(limiter);
+app.use(compression());
+app.use(loggingMiddleware);
 
 // Routes
 app.use("/api", router);
